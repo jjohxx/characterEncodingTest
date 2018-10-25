c8d174b43acf17758249827f06f682e003f0134d
Marek Żurawski <marek.zurawski@algolytics.pl>
2018-10-25 15:11:20 +0200

    Initial commit

---
+++ src/main/java/com/algolytics/test/api/MyRequest.java
@@ -0,0 +1,14 @@
+package com.algolytics.test.api;
+
+public class MyRequest {
+
+    private String field = "";
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+}
---
+++ src/main/java/com/algolytics/test/api/MyResponse.java
@@ -0,0 +1,15 @@
+package com.algolytics.test.api;
+
+public class MyResponse {
+
+    private String fieldPl = "ĄĆĘŁŃÓŚŹŻąćęłńóśźż";
+    private String fieldEn = "ACELNOSZZacelnoszz";
+
+    public String getFieldPl() {
+        return fieldPl;
+    }
+
+    public String getFieldEn() {
+        return fieldEn;
+    }
+}

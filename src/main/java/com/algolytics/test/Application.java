ca032e4369513301d1694249a9968917d8ac7cae
Jhonatan Mamani <jhonatan.mamani@jalasoft.com>
2018-10-29 13:21:27 -0400

    Fix charset

--- src/main/java/com/algolytics/test/Application.java
+++ src/main/java/com/algolytics/test/Application.java
@@ -1,12 +1,26 @@
 package com.algolytics.test;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        jsonConverter.setObjectMapper(objectMapper);
+        jsonConverter.setDefaultCharset(null);
+        return jsonConverter;
     }
 }
